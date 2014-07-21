b73a82354c595b7349995c8d519328e3d6a29c8a
Elizabeth Certa <eliz.certa@gmail.com>
2014-07-21 16:50:30 -0400

    Sample stats to assist playing with the CSS

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -2,11 +2,11 @@ class PlayController < ApplicationController
   def index
     incarnate!
 
-    i = current_profile.current_incarnation
+    i = current_profile.active_incarnation
     
-    @stats
-    @output
-    @choices
+    @stats = [{name: :hitpoints, max: 10, value: 3}, {name: :health, max: 10, value: 5}]
+    @output = ["A paragraph.", "paragraph 2"]
+    @choices = [{id: "choice_id", label: "Choice Label"}]
   end
 
   def update
