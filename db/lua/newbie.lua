436321f99b08e88740a644e165e455421d1fbf95
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-23 15:04:17 -0400

    Improve newbie.lua

--- db/lua/newbie.lua
+++ db/lua/newbie.lua
@@ -1,10 +1,30 @@
+function message(text, button_text)
+  p(text)
+  pause(button_text)
+end
+
+function clear()
+  newpage()
+end
+
+function pause()
+  set_choices({{id="continue", label="Continue"}})
+  wait()
+end
+
+
+
+
 function init()
+    newpage()
     p[[Hello]]
-    coroutine.yield({status='prompt'})
+    pause()
     room()
 end
+
 function room()
+  newpage()
   p[[You are in a room]]
-  coroutine.yield({status='prompt'})
+  pause()
   init()
 end
